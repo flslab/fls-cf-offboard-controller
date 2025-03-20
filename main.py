80270556c4740c1037c4543fba6e6513d24268f2
hamed <amzhamed@gmail.com>
2025-03-20 10:42:58 -0700

    update animation

--- main.py
+++ main.py
@@ -127,13 +127,13 @@ def blender_animation(scf, interval):
 
     for i in range(1, len(animation_data)+1):
         position = animation_data[str(i)]['pos']
-        print('Setting position {}'.format(position))
+        # print('Setting position {}'.format(position))
         for i in range(4):
             cf.commander.send_position_setpoint(position[0],
                                                 position[1],
                                                 position[2],
                                                 0)
-            time.sleep(0.1)
+            time.sleep(0.08)
 
     print("Landing...")
     land(cf, animation_data['120']['pos'])
