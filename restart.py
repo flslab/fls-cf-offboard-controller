d190fa77f57030a97716abc0d1cc6dabee136af0
hamed <amzhamed@gmail.com>
2025-03-20 09:36:52 -0700

    fix restart

--- restart.py
+++ restart.py
@@ -1,26 +1,6 @@
-import time
-import logging
-import cflib
-from cflib.crazyflie import Crazyflie
-from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.utils.power_switch import PowerSwitch
 from cflib.utils import uri_helper
 
-# Use USB connection
-URI = uri_helper.uri_from_env(default='usb://0')
+URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E711')
 
-# Initialize the Crazyflie drivers
-logging.basicConfig(level=logging.ERROR)
-cflib.crtp.init_drivers()
-
-def restart_crazyflie(scf):
-    """Sends a system reset command to reboot the Crazyflie."""
-    print("Restarting Crazyflie...")
-    scf.cf.system.resetsystem()
-    time.sleep(5)  # Wait a few seconds for reboot
-    print("Crazyflie restarted.")
-
-# Connect and restart
-if __name__ == "__main__":
-    print("Connecting to Crazyflie via USB...")
-    with SyncCrazyflie(URI, cf=Crazyflie(rw_cache="./cache")) as scf:
-        restart_crazyflie(scf)
+PowerSwitch(URI).stm_power_cycle()
