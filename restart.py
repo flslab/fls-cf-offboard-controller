7a1031c988c3fc292810aa3bb69c947bbd94f22d
hamed <amzhamed@gmail.com>
2025-08-14 09:34:36 -0700

    refactor restart

--- restart.py
+++ restart.py
@@ -5,41 +5,36 @@ import cflib.crtp
 
 logging.basicConfig(level=logging.ERROR)
 
-URI = 'usb://0'  # Change to 'radio://0/80/2M' for radio link
+URI = 'usb://0'  # or 'radio://0/80/2M'
 
 
-class Rebooter:
-    def __init__(self, uri):
-        self.cf = Crazyflie()
-        self.uri = uri
+def reboot_crazyflie(uri):
+    cflib.crtp.init_drivers(enable_debug_driver=False)
+    cf = Crazyflie()
 
-        self.cf.connected.add_callback(self.connected)
-        self.cf.connection_failed.add_callback(self.connection_failed)
-        self.cf.disconnected.add_callback(self.disconnected)
+    def connected(link_uri):
+        print("Connected to", link_uri)
+        # Wait a bit for parameters to load
+        time.sleep(1.0)
+        print("Sending reboot command...")
+        cf.param.set_value('sys.resetToFirmware', '1')
+        time.sleep(0.2)
+        cf.close_link()
 
-        cflib.crtp.init_drivers(enable_debug_driver=False)
-        print(f"Connecting to {uri}")
-        self.cf.open_link(uri)
+    def connection_failed(link_uri, msg):
+        print("Connection failed:", msg)
 
-    def connected(self, link_uri):
-        print(f"Connected to {link_uri}")
-        # Wait for parameter TOC to be downloaded
-        self.cf.param.add_update_callback(group='sys', name='uptime', cb=self.on_params_ready)
+    def disconnected(link_uri):
+        print("Disconnected from", link_uri)
 
-    def on_params_ready(self, name, value):
-        # Called once we know the parameter system is alive
-        print("Parameter system ready, sending reboot command...")
-        self.cf.param.set_value('sys.resetToFirmware', '1')
-        time.sleep(0.5)
-        self.cf.close_link()
+    cf.connected.add_callback(connected)
+    cf.connection_failed.add_callback(connection_failed)
+    cf.disconnected.add_callback(disconnected)
 
-    def connection_failed(self, link_uri, msg):
-        print(f"Connection failed to {link_uri}: {msg}")
-
-    def disconnected(self, link_uri):
-        print(f"Disconnected from {link_uri}")
+    print("Connecting to", uri)
+    cf.open_link(uri)
+    time.sleep(3)  # give time for reboot before script exits
 
 
 if __name__ == '__main__':
-    Rebooter(URI)
-    time.sleep(5)  # Allow time for connection/reboot before script exits
+    reboot_crazyflie(URI)
