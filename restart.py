e69f181e2d29fe76798b3b7474b5b1e8e54c6bd0
hamed <amzhamed@gmail.com>
2025-08-14 09:38:19 -0700

    refactor restart

--- restart.py
+++ restart.py
@@ -1,7 +1,9 @@
 import logging
 import time
-from cflib.crazyflie import Crazyflie
 import cflib.crtp
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+
 
 logging.basicConfig(level=logging.ERROR)
 
@@ -9,31 +11,18 @@ URI = 'usb://0'  # or 'radio://0/80/2M'
 
 
 def reboot_crazyflie(uri):
-    cflib.crtp.init_drivers(enable_debug_driver=False)
-    cf = Crazyflie()
+    cflib.crtp.init_drivers(enable_serial_driver=True)
 
-    def connected(link_uri):
-        print("Connected to", link_uri)
+    with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
+        cf = scf.cf
+
+        print("Connected to", uri)
         # Wait a bit for parameters to load
         time.sleep(1.0)
         print("Sending reboot command...")
         cf.param.set_value('sys.resetToFirmware', '1')
         time.sleep(0.2)
         cf.close_link()
-
-    def connection_failed(link_uri, msg):
-        print("Connection failed:", msg)
-
-    def disconnected(link_uri):
-        print("Disconnected from", link_uri)
-
-    cf.connected.add_callback(connected)
-    cf.connection_failed.add_callback(connection_failed)
-    cf.disconnected.add_callback(disconnected)
-
-    print("Connecting to", uri)
-    cf.open_link(uri)
-    time.sleep(3)  # give time for reboot before script exits
 
 
 if __name__ == '__main__':
