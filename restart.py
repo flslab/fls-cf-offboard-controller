e1d1520963ca7c47e99da1d18c7f91b29eaed66b
hamed <amzhamed@gmail.com>
2025-08-14 09:48:39 -0700

    refactor restart

--- restart.py
+++ restart.py
@@ -1,9 +1,7 @@
 import logging
 import time
 import cflib.crtp
-from cflib.crazyflie import Crazyflie
-from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
-
+from cflib.utils.power_switch import PowerSwitch
 
 logging.basicConfig(level=logging.ERROR)
 
@@ -12,17 +10,7 @@ URI = 'usb://0'  # or 'radio://0/80/2M'
 
 def reboot_crazyflie(uri):
     cflib.crtp.init_drivers(enable_serial_driver=True)
-
-    with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
-        cf = scf.cf
-
-        print("Connected to", uri)
-        # Wait a bit for parameters to load
-        time.sleep(1.0)
-        print("Sending reboot command...")
-        cf.param.set_value('sys.resetToFirmware', '1')
-        time.sleep(0.2)
-        cf.close_link()
+    PowerSwitch(uri).stm_power_cycle()
 
 
 if __name__ == '__main__':
