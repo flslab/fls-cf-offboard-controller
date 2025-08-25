0497c2d140233f59079719a371a83c1b4a57524a
hamed <amzhamed@gmail.com>
2025-08-25 11:25:11 -0700

    localize

--- controller.py
+++ controller.py
@@ -618,27 +618,26 @@ class LocalizationWrapper(Thread):
         super().__init__()
         self.cf = cf
         self.stopped = False
-        # Size of the Position structure: 1 byte for 'valid' + 7 floats (4 bytes each)
-        self.position_size = 1 + 7 * 4  # 1 byte + 7 floats (4 bytes each)
+        self.position_size = 1 + 3 + 6 * 4  # 1 byte + 3 byte padding + 6 floats (4 bytes each)
         self.shm_name = "/pos_shared_mem"
         self.shm_fd = open(f"/dev/shm{self.shm_name}", "r+b")  # Open shared memory
         self.shm_map = mmap.mmap(self.shm_fd.fileno(), self.position_size, access=mmap.ACCESS_READ)
 
     def run(self):
         while not self.stopped:
-            data = self.shm_map[:self.position_size] # Read 8 bytes (bool + 7 floats = 1 byte + 28 bytes)
+            data = self.shm_map[:self.position_size]  # Read 8 bytes (bool + 7 floats = 1 byte + 28 bytes)
             # print("raw data:", data)
-            valid = struct.unpack("<?", data[:1])[0]  # Extract the validity flag (1 byte)
+            valid = struct.unpack("<4?", data[:4])[0]  # Extract the validity flag (1 byte)
 
             if valid:
-                x, y, z, qx, qy, qz, qw = struct.unpack("<7f", data[1:])
-                print(f"Position: ({-y}, {-x}, {z-0.05}), Orientation: ({qx}, {qy}, {qz}, {qw})")
+                x, y, z, roll, pitch, yaw = struct.unpack("<6f", data[4:28])
+                print(f"Position: ({-y}, {-x}, {z-0.05})")
                 send_extpose_quat(self.cf, -y, -x, z)
             else:
                 pass
                 # print("Invalid data received")
 
-            time.sleep(1/60)
+            time.sleep(1/120)
 
     def stop(self):
         self.stopped = True
