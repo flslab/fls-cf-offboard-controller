a0e46ae7183da907247e0e7c2a1331bad3c3b164
hamed <amzhamed@gmail.com>
2025-11-15 16:55:24 -0800

    led

--- led.py
+++ led.py
@@ -1,60 +1,86 @@
-#!/usr/bin/env python3
-# NeoPixel library strandtest example
-# Author: Tony DiCola (tony@tonydicola.com)
-#
-# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
-# various animations on a strip of NeoPixels.
-
 import time
-from rpi_ws281x import *
-import json
-import argparse
-
-# LED strip configuration:
-LED_COUNT      = 60     # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 10      # DMA channel to use for generating a signal (try 10)
-LED_BRIGHTNESS = 65      # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+import threading
+import board
+import neopixel_spi as neopixel
 
 
-class LED:
-    def __init__(self):
-        self.strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-        self.strip.begin()
+class MovingDotLED(threading.Thread):
+    def __init__(self, num_pixels=72, color=(227, 253, 255), tail_decay=0.75, delay=0.02, brightness=1.0):
+        super().__init__()
+        self.num_pixels = num_pixels
+        self.color = color
+        self.tail_decay = tail_decay
+        self.delay = delay
+        self.leds = [(0, 0, 0)] * self.num_pixels
+        self.running = False
 
-    def set_frame(self, brightness_map):
-        for idx, brightness in brightness_map.items():
-            b = brightness_map[idx] / 10
-            self.strip.setPixelColor(int(idx), Color(int(b*227), int(b*253), int(b*255)))
-        self.strip.show()
+        # Setup SPI and NeoPixel strip
+        self.pixels = neopixel.NeoPixel_SPI(
+            board.SPI(),
+            self.num_pixels,
+            pixel_order=neopixel.GRB,
+            auto_write=False,
+            brightness=brightness
+        )
 
+    def fade_tail(self):
+        for i in range(self.num_pixels):
+            r, g, b = self.leds[i]
+            self.leds[i] = (
+                int(r * self.tail_decay),
+                int(g * self.tail_decay),
+                int(b * self.tail_decay)
+            )
 
-# Define functions which animate LEDs in various ways.
-    def colorWipe(self, color, wait_ms=50):
-        """Wipe color across display a pixel at a time."""
-        for i in range(self.strip.numPixels()):
-            self.strip.setPixelColor(i, color)
-            self.strip.show()
-            time.sleep(wait_ms/1000.0)
+    def draw_frame(self):
+        for i, color in enumerate(self.leds):
+            self.pixels[i] = color
+        self.pixels.show()
+
+    def turn_on(self):
+        # self.leds[0] = self.color
+        self.leds[11] = self.color
+        # self.leds[22] = self.color
+        self.leds[34] = self.color
+        self.draw_frame()
+
+    def run(self):
+        self.running = True
+
+        # # self.leds[0] = self.color
+        # self.leds[11] = self.color
+        # # self.leds[22] = self.color
+        # self.leds[34] = self.color
+        # self.draw_frame()
+
+    def show_single_color(self):
+        for pos in range(self.num_pixels):
+            self.leds[pos] = self.color
+        self.draw_frame()
+
+    def halo_loop(self):
+        while self.running:
+            for pos in range(self.num_pixels):
+                if not self.running:
+                    break
+                self.fade_tail()
+                self.leds[pos] = self.color
+                self.draw_frame()
+                time.sleep(self.delay)
+
+    def stop(self):
+        self.running = False
+        self.join()
+        self.clear()
 
     def clear(self):
-        self.colorWipe(Color(0, 0, 0), 10)
+        self.leds = [(0, 0, 0)] * self.num_pixels
+        self.draw_frame()
+
 
 if __name__ == '__main__':
-    led = LED()
-
-    # led.colorWipe(Color(227,253,255))
-
-    with open("animation_data.json", "r") as f:
-        animation_data = json.load(f)
-
-    for i in range(5):
-        for frame, data in animation_data.items():
-            led.set_frame(data['led'])
-            time.sleep(1/24)
-
-    led.clear()
+    led = MovingDotLED()
+    led.start()
+    led.show_single_color()
+    time.sleep(10)
+    led.stop()
