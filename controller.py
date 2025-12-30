4359bd50b935404ff6f0e47a9880f6819930d7f9
hamed <amzhamed@gmail.com>
2025-12-30 11:29:20 -0800

    tune vel

--- controller.py
+++ controller.py
@@ -66,26 +66,46 @@ z_filter_alpha = 0.3
 
 _time = []
 log_vars = {
-    "controller.pitch": {
+    "ctrltarget.vx": {
         "type": "float",
-        "unit": "deg",
+        "unit": "m/s",
         "data": [],
     },
-    "stateEstimate.pitch": {
+    "stateEstimate.vx": {
         "type": "float",
-        "unit": "deg",
+        "unit": "m/s",
         "data": [],
     },
-    "controller.roll": {
+    "ctrltarget.vy": {
         "type": "float",
-        "unit": "deg",
+        "unit": "m/s",
         "data": [],
     },
-    "stateEstimate.roll": {
+    "stateEstimate.vy": {
         "type": "float",
-        "unit": "deg",
+        "unit": "m/s",
         "data": [],
     },
+    # "controller.pitch": {
+    #     "type": "float",
+    #     "unit": "deg",
+    #     "data": [],
+    # },
+    # "stateEstimate.pitch": {
+    #     "type": "float",
+    #     "unit": "deg",
+    #     "data": [],
+    # },
+    # "controller.roll": {
+    #     "type": "float",
+    #     "unit": "deg",
+    #     "data": [],
+    # },
+    # "stateEstimate.roll": {
+    #     "type": "float",
+    #     "unit": "deg",
+    #     "data": [],
+    # },
     # "controller.pitchRate": {
     #     "type": "float",
     #     "unit": "rad/s",
@@ -451,7 +471,7 @@ def set_pid_values(scf, propeller_size=None, with_cage=False):
         # cf.param.set_value('velCtlPid.vzKi', '2.0')
         # cf.param.set_value('velCtlPid.vzKd', '0.5')
         #
-        cf.param.set_value('pid_attitude.roll_kp', '5.0')
+        cf.param.set_value('pid_attitude.roll_kp', '6.0')
         cf.param.set_value('pid_attitude.roll_ki', '4.0')
         cf.param.set_value('pid_attitude.roll_kd', '0.0')
         cf.param.set_value('pid_attitude.pitch_kp', '6.0')
