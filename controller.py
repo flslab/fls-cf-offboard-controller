93d5bff6c4fa9d5f3328fbbf03dc2b25347051a3
hamed <amzhamed@gmail.com>
2025-12-29 16:28:58 -0800

    add xy tune

--- controller.py
+++ controller.py
@@ -239,19 +239,19 @@ def xy_tune_pattern(scf):
 
     commander.takeoff(1.0, 2.0)
     time.sleep(3)
-    flight_time = 2
+    flight_time = 1
 
-    commander.go_to(-1, 0, 0, 0, flight_time, relative=True)
-    time.sleep(flight_time)
+    commander.go_to(0, 0, 0, 0, flight_time, relative=True)
+    time.sleep(flight_time+10)
 
-    commander.go_to(0, 1, 0, 0, flight_time, relative=True)
-    time.sleep(flight_time)
-
-    commander.go_to(-1, 0, 0, 0, flight_time, relative=True)
-    time.sleep(flight_time)
-
-    commander.go_to(0, -1, 0, 0, flight_time, relative=True)
-    time.sleep(flight_time)
+    # commander.go_to(0, 1, 0, 0, flight_time, relative=True)
+    # time.sleep(flight_time)
+    #
+    # commander.go_to(-1, 0, 0, 0, flight_time, relative=True)
+    # time.sleep(flight_time)
+    #
+    # commander.go_to(0, -1, 0, 0, flight_time, relative=True)
+    # time.sleep(flight_time)
 
     commander.land(0.0, 2.0)
     time.sleep(2)
