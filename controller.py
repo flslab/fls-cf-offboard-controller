a28a47d9165d21cb21f8651e5394df9e47f2d5b0
hamed <amzhamed@gmail.com>
2025-12-30 14:53:36 -0800

    tune z

--- controller.py
+++ controller.py
@@ -514,7 +514,7 @@ def set_pid_values(scf, propeller_size=None, with_cage=False):
         cf.param.set_value('posCtlPid.yKp', '2.1')
         cf.param.set_value('posCtlPid.yKi', '0.1')
         cf.param.set_value('posCtlPid.yKd', '0.0')
-        cf.param.set_value('posCtlPid.zKp', '1.9')
+        cf.param.set_value('posCtlPid.zKp', '1.85')
         cf.param.set_value('posCtlPid.zKi', '2.0')
         cf.param.set_value('posCtlPid.zKd', '0.05')
         cf.param.set_value('posCtlPid.thrustMin', '12000')
