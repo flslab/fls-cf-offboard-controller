4c821ecc3cdcbaf84f4a8cdf827bf2eb8a96784f
hamed <amzhamed@gmail.com>
2026-01-13 19:22:18 -0800

    temp

--- servo_pwm.py
+++ servo_pwm.py
@@ -24,7 +24,7 @@ class Servo:
         self.offsets = offsets
         self.values = [None, None]
         self.tick_angle = 1
-        self.tick_duration = 1/50
+        self.tick_duration = 1/45
 
     def set(self, index, a):
         angle = a + self.offsets[index]
