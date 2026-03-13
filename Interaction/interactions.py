f93ec8e70691df5d61cc186acc117cbcdb90f1f4
Shuqin <shuqinzhu37@gmail.com>
2026-03-13 09:40:28 -0700

    update

--- Interaction/interactions.py
+++ Interaction/interactions.py
@@ -192,16 +192,31 @@ class InteractionsControl:
                         hover_pos = target_pos
                         status = 3
 
-                        log_data = {"vel": speed, 'Pos': list(pos), 'Target': list(hover_pos)}
+                        log_data = {
+                            "vel": round(speed, 3),
+                            "Pos": [round(x, 3) for x in pos],
+                            "Target": [round(x, 3) for x in hover_pos]
+                        }
                         self._log_event("User Disengage", log_data)
+                        continue
                     else:
                         logger.info(f"Switching to Hover From {status}.")
                         hover_pos = target_pos
                         status = 3
-                        log_data = {"vel": speed, 'Pos': list(pos), 'Target': list(hover_pos)}
-                        self._log_event("User Disengage", log_data)
 
-                log_data = {"vel": speed, 'Pos': list(pos), 'Target': list(target_pos)}
+                        log_data = {
+                            "vel": round(speed, 3),
+                            "Pos": [round(x, 3) for x in pos],
+                            "Target": [round(x, 3) for x in target_pos]
+                        }
+                        self._log_event("User Disengage", log_data)
+                        continue
+
+                log_data = {
+                    "vel": round(speed, 3),
+                    "Pos": [round(x, 3) for x in pos],
+                    "Target": [round(x, 3) for x in target_pos]
+                }
                 self._log_event("User Pushing", log_data)
 
                 if base_attitude < 0:
