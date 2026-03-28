6646d3a68efe810ab553d08d1926d8f1ace126f5
Shuqin <shuqinzhu37@gmail.com>
2026-03-28 16:44:06 -0700

    user study

--- orchestrator.py
+++ orchestrator.py
@@ -154,7 +154,7 @@ class SwarmOrchestrator:
             f"--takeoff-altitude {alt} "
             "--smooth-controller-rate 50 "
             "--log "
-            "--skip-takeoff --skip-landing "
+            # "--skip-takeoff --skip-landing "
             f"> drone_{drone['id']}.log 2>&1 < /dev/null &",
         ]
 
