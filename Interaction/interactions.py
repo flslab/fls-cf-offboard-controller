60f79fa2c9aafa403d734874472f2b843bc4434e
Shuqin <shuqinzhu37@gmail.com>
2026-05-01 13:22:53 -0700

    test reset without switching controller

--- Interaction/interactions.py
+++ Interaction/interactions.py
@@ -877,7 +877,6 @@ class InteractionsControl:
                     self.cf.param.set_value_raw('stabilizer.controller', 0x08, 1)
                     v_virtual = np.zeros(3)
                     prev_interact_vel = vel.copy()
-                    self.lo_commander.send_zdistance_setpoint(0, 0, 0, pos[2] + 0.02)
                     self._safe_sleep(0.01)
                     continue
                 else:
@@ -963,7 +962,7 @@ class InteractionsControl:
                     }
                     self._log_event("User Pushing", log_data)
                     yaw_rate_cmd = max(min(-5.0 * current_yaw, 50.0), -50.0)
-                    self.lo_commander.send_zdistance_setpoint(target_roll, target_pitch, yaw_rate_cmd, target_pos[2])
+                    self.lo_commander.send_zdistance_setpoint(target_roll, target_pitch, yaw_rate_cmd, target_pos[2] + 0.02)
 
             elif status == 2:  # coasting
                 if blender_state is not None:
