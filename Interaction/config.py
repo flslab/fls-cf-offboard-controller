cf63ff9d3884f53ca849c54b146cb67f52c42b9a
Shuqin <shuqinzhu37@gmail.com>
2026-09-10 14:12:38 -0700

    update

--- Interaction/config.py
+++ Interaction/config.py
@@ -684,10 +684,10 @@ CTL_I_D = {
 LOG_VARS = {
     'VEL_ORI': VEL_ORI,
     'POS_ACC': POS_ACC,
-    'RATE_EST': RATE_EST,
-    'YAW_CTL': YAW_CTL,
-    'MOT_BAT': MOT_BAT,
-    'POS_CTL_I_D': CTL_I_D,
+    # 'RATE_EST': RATE_EST,
+    # 'YAW_CTL': YAW_CTL,
+    # 'MOT_BAT': MOT_BAT,
+    # 'POS_CTL_I_D': CTL_I_D,
     'POS_VEL_CTL': POS_VEL_CTL,
     'ATT_RATE_CTL': ATT_RATE_CTL,
 }
