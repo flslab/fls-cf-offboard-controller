366cbea4b8e5cef349e65355dd2c137f312bc66d
hamed <amzhamed@gmail.com>
2026-09-15 15:40:18 -0700

    fix

--- controller.py
+++ controller.py
@@ -2222,7 +2222,7 @@ class Controller:
 
         for _ in range(iterations):
             if iterations > 1:
-                if len(waypoints):
+                if len(pointers):
                     self.smooth_controller.set_group_values("pointers", pointers[0], duration=0)
                 sleep_duration = self.animation_start_times[mission_index] + elapsed_time + 0.1 - time.time()
                 elapsed_time += 0.1
